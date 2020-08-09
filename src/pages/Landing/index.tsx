ed4d44a241bfc347eb6d5dfc73e3cf7e35f448ba
Alcimerio Rangel <alcimerioj@id.uff.br>
2020-08-09 17:10:34 -0300

    Add tab nagicatino in Study page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
     navigate('GiveClasses');
   }
 
+  function handleNavigateToStudyPages() {
+    navigate('Study');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -27,15 +31,19 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
-          <Image source={studyIcon} />
-          <Text style={styles.buttonText}>Estudar</Text>
+
+        <RectButton 
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}>
+            <Image source={studyIcon} />
+            <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
+
         <RectButton
           onPress={handleNavigateToGiveClassesPage}
           style={[styles.button, styles.buttonSecondary]}>
-          <Image source={giveClassesIcon} />
-          <Text style={styles.buttonText}>Dar aulas</Text>
+            <Image source={giveClassesIcon} />
+            <Text style={styles.buttonText}>Dar aulas</Text>
         </RectButton>
       </View>
 
